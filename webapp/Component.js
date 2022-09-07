91e78afaed83854c831f5e7a08e64a9cf7e8de32
Jorj <jorj.petrosyan@urartian.am>
2022-09-07 16:56:29 +0400

    The app get data from northwind

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,6 +1,7 @@
 sap.ui.define([
-    'sap/ui/core/UIComponent'
-], function (UIComponent) {
+    'sap/ui/core/UIComponent',
+    'sap/ui/model/json/JSONModel'
+], function (UIComponent, JSONModel) {
     'use strict';
     return UIComponent.extend('sap.app.Component', {
         metadata: {
@@ -8,6 +9,21 @@ sap.ui.define([
         }, 
         init: function () {
             UIComponent.prototype.init.apply(this, arguments);
+            
+            let oData = this.getModel('invoice');
+            console.log(oData);
+            oData.read("/Employees", {
+                success: function (response) {
+                    // response = Object.assign({}, response['results']);
+                    // oData = {employee: response}
+                },
+                error: function (oError) {
+
+                }
+            })
+
+            // this.setModel(oData, 'employees');
+            // console.log(this.getModel('employees'));
         }
     });
 });
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,21 +1,26 @@
 sap.ui.define([
+    'sap/m/Text',
     'sap/ui/core/mvc/Controller',
     'sap/ui/model/json/JSONModel'
-], function (Controller, JSONModel) {
+], function (Text, Controller, JSONModel) {
     'use strict';
     return Controller.extend('sap.app.controller.App', {
         addUser: function () {
-            let roleSelect = this.getView().byId('roleSelect');
-            console.log(roleSelect);
+            
+
+
+            // let roleSelect = this.getView().byId('roleSelect');
+            // console.log(roleSelect);
             
             
-            let oData = this.getView().getModel('userModel')['oData'];
-            let oModel = new JSONModel(oData);
-            this.getView().setModel(oModel)
+            // let oData = this.getView().getModel('userModel')['oData'];
+            // let oModel = new JSONModel(oData);
+            // this.getView().setModel(oModel)
             
-            console.log(this.getView().getModel('userModel'));
+            // console.log(this.getView().getModel('userModel'));
             
             
         }
+        // ['oMetadata']['oMetadata']['dataServices']['schema'][0]['entityType']
     });
 });
